3fd52f96f170a02d26902372008e9c3139f4d3e1
Hallmann <michelle-celina.hallmann@stud.hshl.de>
2021-02-07 21:58:19 +0100

    auskommentierter Code Versuch Problemlösung

--- public/jsFiles/player.js
+++ public/jsFiles/player.js
@@ -1,16 +1,30 @@
 let direction = "right";
 
+// Versuch: Lösen des Problems der Browserkompabilität (Bugs mit Firefox)
+/*let pressUp;
+let lastPressUp;
+let pressRight;
+let lastPressRight;
+let counterRight;
+let firstIterationRight;
+
+let lastPressLeft;
+let pressLeft;
+let counterLeft;
+let firstIterationLeft;*/
+
+
 export default class Player {
 
     constructor(scene, x, y) {
         this.scene = scene;
 
-        // Animation
+        // Animation of the player
         const anims = scene.anims;
 
         anims.create({
             key: "left",
-            frames: anims.generateFrameNumbers("player", { start: 0, end: 4 }),
+            frames: anims.generateFrameNumbers("player", {start: 0, end: 4}),
             frameRate: 8,
             repeat: -1
         });
@@ -43,6 +57,7 @@ export default class Player {
 
         // Keys
         const {LEFT, RIGHT, UP, W, A, D, SPACE} = Phaser.Input.Keyboard.KeyCodes;
+
         this.keys = scene.input.keyboard.addKeys({
             left: LEFT,
             right: RIGHT,
@@ -52,7 +67,16 @@ export default class Player {
             d: D,
             space: SPACE
         });
+
+        // Versuch: Lösen des Problems der Browserkompabilität (Bugs mit Firefox)
+       /* counterLeft = 0;
+        counterRight = 0;
+
+        firstIterationLeft = true;
+        firstIterationRight = true;*/
+
     }
+
     freeze() {
         this.sprite.body.moves = false;
     }
@@ -60,34 +84,58 @@ export default class Player {
     update() {
         const {keys, sprite} = this;
         const onGround = sprite.body.blocked.down;
+        let pressUp;
 
-        if (keys.left.isDown || keys.a.isDown) {
-            sprite.setVelocityX(-160);
-            direction = "left";
-           // console.log(sprite.body.velocity.x);
+         if (keys.left.isDown || keys.a.isDown) {
 
-            if(onGround)
-            {sprite.anims.play('left', true);}
+             sprite.setVelocityX(-160);
+             direction = "left";
+            // console.log(sprite.body.velocity.x);
 
-        } else if (keys.right.isDown || keys.d.isDown) {
-            sprite.setVelocityX(160);
-            direction = "right";
+             if(onGround)
+             {sprite.anims.play('left', true);}
 
-            if (onGround)
-            {sprite.anims.play('right', true);}
+         } else if (keys.right.isDown || keys.d.isDown) {
+             sprite.setVelocityX(160);
+             direction = "right";
+
+             if (onGround)
+             {sprite.anims.play('right', true);}
+
+         } else {
+             sprite.setVelocityX(0);
+             if(direction === 'right') {
+                 sprite.anims.play('turnFromRight');
+             } else {
+                 sprite.anims.play('turnFromLeft');
+             }
+         }
+
+        // Versuch: Lösen des Problems der Browserkompabilität (Bugs mit Firefox)
+        /*pressLeft = keys.a.repeats;
+       pressRight = keys.d.repeats;
+       pressUp = keys.w.repeats;*/
+
+        //console.log(pressLeft);
+      /*  if (keys.left.isDown || keys.a.isDown){
+             this.checkLeft(onGround, keys);
+        }
+        else if (keys.right.isDown || keys.d.isDown) {
+            this.checkRight(onGround);
 
         } else {
             sprite.setVelocityX(0);
-            if(direction === 'right') {
+            if (direction === 'right') {
                 sprite.anims.play('turnFromRight');
             } else {
                 sprite.anims.play('turnFromLeft');
             }
-        }
+        }*/
 
 
-        if (onGround && (keys.up.isDown || keys.w.isDown || keys.space.isDown)) {
+        if (onGround && (keys.up.isDown || keys.w.isDown || keys.space.isDown) /* && (lastPressUp !== pressUp || pressUp === 0 ) */ ) {
             sprite.setVelocityY(-380);
+
         }
 
             if(!onGround){
@@ -98,8 +146,92 @@ export default class Player {
             }
         }
 
+            // Versuch: Lösen des Problems der Browserkompabilität (Bugs mit Firefox)
+           // console.log(pressUp);
+           // lastPressUp = pressUp;
+/*
+            if(!this.keys.a.isDown){
+                counterLeft = 0;
+                firstIterationLeft = true;
+            }
+        if(!this.keys.d.isDown){
+            counterRight = 0;
+            firstIterationRight = true;
+        }*/
+
         return direction;
     }
+
+    // Versuch: Lösen des Problems der Browserkompabilität (Bugs mit Firefox)
+   /* checkLeft(onGround){
+        if(!onGround){
+            counterLeft = 40;
+            firstIterationLeft = 40;
+        }
+        if (counterLeft == 0){
+            if(firstIterationLeft){
+                counterLeft = 40;
+                firstIterationLeft = false;
+                lastPressLeft = pressLeft -1;
+            }else {
+            counterLeft = 7;
+            lastPressLeft = pressLeft;
+            }
+        }
+        if (counterLeft == 1){
+            console.log("onGround: " + onGround);
+            if(lastPressLeft == pressLeft && onGround){
+                console.log("gleiche Werte");
+                this.keys.a.isDown = false;
+                firstIterationLeft = true;
+                counterLeft = 0;
+                return;
+            }else{counterLeft = 1;}
+        }
+
+            this.sprite.setVelocityX(-160);
+            direction = "left";
+
+            if(onGround)
+            {this.sprite.anims.play('left', true);}
+
+            counterLeft = counterLeft - 1;
+    }
+
+
+        checkRight(onGround){
+        if(!onGround){counterRight = 100;
+        firstIterationRight = true;}
+            if (counterRight == 0){
+                if(firstIterationRight){
+                    counterRight = 40;
+                    firstIterationRight = false;
+                    lastPressRight = pressRight -1;
+                }else counterRight = 7;
+                lastPressRight = pressRight;
+            }
+
+            if (counterRight == 1){
+                if(lastPressRight == pressRight){
+                    console.log("gleiche Werte Rechts");
+                    this.keys.d.isDown = false;
+                    counterRight = 0;
+                    return;
+                }else{
+                    counterRight = 1;
+                }
+            }
+                this.sprite.setVelocityX(160);
+                direction = "right";
+
+                if (onGround)
+                {this.sprite.anims.play('right', true);}
+
+            counterRight = counterRight - 1;
+
+            }
+*/
+
     destroy() {
         this.sprite.destroy();
     }
